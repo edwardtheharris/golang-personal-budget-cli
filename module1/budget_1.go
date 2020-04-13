c27162f794dcbf56ff4fed43ce4072d919a3ed4a
Xander Harris <xander.harris@infor.com>
2020-04-13 15:55:50 -0700

    Add fields to structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,4 +2,14 @@ package module1
 
 // Budget stores budget information
 
+type Budget struct {
+	float32 Max
+	Items   []Item
+}
+
 // Item stores item information
+
+type Item struct {
+	string  Description
+	float32 Price
+}
