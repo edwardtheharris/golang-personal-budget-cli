f7bc613468a4571f4eb4c780466a7a0dcedff0bc
Xander Harris <xander.harris@infor.com>
2020-04-13 15:58:44 -0700

    Add correct fields

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -3,13 +3,13 @@ package module1
 // Budget stores budget information
 
 type Budget struct {
-	float32 Max
-	Items   []Item
+	Max   float32
+	Items []Item
 }
 
 // Item stores item information
 
 type Item struct {
-	string  Description
-	float32 Price
+	Description string
+	Price       float32
 }
